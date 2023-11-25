c6e6c5e725129b466ae811e1474c1c7eae49b6cc
Mathias <mathias.scroccaro@gmail.com>
2023-11-25 17:21:21 -0300

    Handle command arguments

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	outputDir := "./docs"
-
-	BuildStaticPortfolio(outputDir)
-
+func initServer(outputDir string) {
 	fs := http.FileServer(http.Dir(outputDir))
 	http.Handle("/", fs)
 
-	fmt.Print("Listening on http://localhost:3000")
+	fmt.Println("Listening on http://localhost:3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func main() {
+	var outputDir string
+	var server bool
+
+	flag.StringVar(&outputDir, "o", "./docs", "output directory")
+	flag.BoolVar(&server, "s", false, "run server")
+
+	flag.Parse()
+
+	fmt.Println("Outputing to " + outputDir)
+	BuildStaticPortfolio(outputDir)
+
+	if server {
+		initServer(outputDir)
 	}
 }
